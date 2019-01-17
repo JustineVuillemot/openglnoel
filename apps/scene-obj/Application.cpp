1905d627e92dce0c4ff5f7008ec0d7ad6028936b
JUIF ETIENNE <ejuif@localhost.localdomain>
2019-01-17 11:26:33 +0100

    Obj success with normalShader

---
+++ apps/scene-obj/Application.cpp
@@ -0,0 +1,145 @@
+#include "Application.hpp"
+
+#include <iostream>
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+int Application::run()
+{
+	// Put here code to run before rendering loop
+    glClearColor(1,0,0,1);
+
+    // Loop until the user closes the window
+    for (auto iterationCount = 0u; !m_GLFWHandle.shouldClose(); ++iterationCount)
+    {
+        const auto seconds = glfwGetTime();
+
+        // Put here rendering code
+        const auto fbSize = m_GLFWHandle.framebufferSize();
+        glViewport(0, 0, fbSize.x, fbSize.y);
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        //Matrix
+        MVMatrix = glm::translate(view.getViewMatrix(), glm::vec3(0.0f, 0.0f, -5.0f));
+        NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
+        //Envoie des matrices
+        glUniformMatrix4fv( modelViewProjMatrix, 1, GL_FALSE, glm::value_ptr( ProjMatrix * MVMatrix ));
+        glUniformMatrix4fv( modelViewMatrix, 1, GL_FALSE, glm::value_ptr( MVMatrix ));
+        glUniformMatrix4fv( normalMatrix, 1, GL_FALSE, glm::value_ptr( NormalMatrix ));
+
+        glBindVertexArray(vao);
+        auto indexOffset = 0;
+        for (const auto indexCount: data.indexCountPerShape)
+        {
+            glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, (const GLvoid*) (indexOffset * sizeof(GLuint)));
+            indexOffset += indexCount;
+        }
+
+        // GUI code:
+		glmlv::imguiNewFrame();
+
+        {
+            ImGui::Begin("GUI");
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+		glmlv::imguiRenderFrame();
+
+        glfwPollEvents(); // Poll for and process events
+
+        auto ellapsedTime = glfwGetTime() - seconds;
+        auto guiHasFocus = ImGui::GetIO().WantCaptureMouse || ImGui::GetIO().WantCaptureKeyboard;
+        if (!guiHasFocus) {
+            // Put here code to handle user interactions
+            view.update(ellapsedTime);
+        }
+
+		m_GLFWHandle.swapBuffers(); // Swap front and back buffers
+    }
+
+    return 0;
+}
+
+Application::Application(int argc, char** argv):
+    m_AppPath { glmlv::fs::path{ argv[0] } },
+    m_AppName { m_AppPath.stem().string() },
+    m_ImGuiIniFilename { m_AppName + ".imgui.ini" },
+    m_ShadersRootPath { m_AppPath.parent_path() / "shaders" },
+    m_AssetsRootPath { m_AppPath.parent_path() / "assets" },
+    view { glmlv::ViewController(m_GLFWHandle.window(), 10) }
+{
+    ImGui::GetIO().IniFilename = m_ImGuiIniFilename.c_str(); // At exit, ImGUI will store its windows positions in this file
+
+    // Put here initialization code
+    //PROGRAM
+    const auto pathToSMVS = m_ShadersRootPath / m_AppName / "scene.vs.glsl";
+    const auto pathToSMFS = m_ShadersRootPath / m_AppName / "scene.fs.glsl";
+
+    program = glmlv::compileProgram({pathToSMVS, pathToSMFS});
+    program.use();
+
+
+    glmlv::loadObjScene(m_AssetsRootPath / m_AppName / "sponza/sponza.obj", data);
+
+    const auto sceneDiagonalSize = glm::length(data.bboxMax - data.bboxMin);
+    view.setSpeed(sceneDiagonalSize * 0.1f); // 10% de la scene parcouru par seconde
+
+    sceneSize = glm::length(data.bboxMax - data.bboxMin);
+    ProjMatrix = glm::perspective(70.f, float(m_nWindowWidth) / m_nWindowHeight, 0.01f * sceneSize, sceneSize); // near = 1% de la taille de la scene, far = 100%
+
+
+    glEnable(GL_DEPTH_TEST);
+
+    const GLuint VERTEX_ATTR_POSITION = 0;
+    const GLuint VERTEX_ATTR_NORMAL = 1;
+    const GLuint VERTEX_ATTR_TEXCOORDS = 2;
+
+    //Construction cube
+    
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ibo);
+    glGenVertexArrays(1, &vao);
+
+    //VBO
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferStorage(GL_ARRAY_BUFFER, sizeof(glmlv::Vertex3f3f2f)*data.vertexBuffer.size(), data.vertexBuffer.data(), 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    //IBO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferStorage(GL_ELEMENT_ARRAY_BUFFER, data.indexBuffer.size() * sizeof(uint32_t), data.indexBuffer.data(), 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    //VAO
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+
+    glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
+    glEnableVertexAttribArray(VERTEX_ATTR_NORMAL);
+    glEnableVertexAttribArray(VERTEX_ATTR_TEXCOORDS);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(VERTEX_ATTR_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(glmlv::Vertex3f3f2f), (const GLvoid*) offsetof(glmlv::Vertex3f3f2f, position));
+    glVertexAttribPointer(VERTEX_ATTR_NORMAL, 3, GL_FLOAT, GL_FALSE, sizeof(glmlv::Vertex3f3f2f), (const GLvoid*) offsetof(glmlv::Vertex3f3f2f, normal));
+    glVertexAttribPointer(VERTEX_ATTR_TEXCOORDS, 2, GL_FLOAT, GL_FALSE, sizeof(glmlv::Vertex3f3f2f), (const GLvoid*) offsetof(glmlv::Vertex3f3f2f, texCoords));
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+
+    //getUniform
+    //MATRIX
+    modelViewProjMatrix = program.getUniformLocation("uModelViewProjMatrix");
+    modelViewMatrix = program.getUniformLocation("uModelViewMatrix");
+    normalMatrix = program.getUniformLocation("uNormalMatrix");
+
+    //LIGHT
+    /*directionalLightDir = program.getUniformLocation("uDirectionalLightDir");
+    directionalLightIntensity = program.getUniformLocation("uDirectionalLightIntensity");
+    pointLightPosition = program.getUniformLocation("uPointLightPosition");
+    pointLightIntensity = program.getUniformLocation("uPointLightIntensity");
+    uKd = program.getUniformLocation("uKd");*/
+}
---
+++ apps/scene-obj/Application.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <glmlv/filesystem.hpp>
+#include <glmlv/GLFWHandle.hpp>
+#include <glmlv/GLProgram.hpp>
+#include <glmlv/ViewController.hpp>
+#include <glmlv/simple_geometry.hpp>
+#include <glmlv/scene_loading.hpp>
+
+class Application
+{
+public:
+    Application(int argc, char** argv);
+
+    int run();
+private:
+    const size_t m_nWindowWidth = 1280;
+    const size_t m_nWindowHeight = 720;
+    glmlv::GLFWHandle m_GLFWHandle{ m_nWindowWidth, m_nWindowHeight, "Template" }; // Note: the handle must be declared before the creation of any object managing OpenGL resource (e.g. GLProgram, GLShader)
+
+    const glmlv::fs::path m_AppPath;
+    const std::string m_AppName;
+    const std::string m_ImGuiIniFilename;
+    const glmlv::fs::path m_ShadersRootPath;
+    const glmlv::fs::path m_AssetsRootPath;
+
+    glmlv::SceneData data;
+    GLuint vbo, vao, ibo;
+
+    glmlv::ViewController view;
+
+    float sceneSize;
+
+    glmlv::GLProgram program;
+
+    GLint modelViewProjMatrix;
+    GLint modelViewMatrix;
+    GLint normalMatrix;
+
+    GLint directionalLightDir;
+    GLint directionalLightIntensity;
+    GLint pointLightPosition;
+    GLint pointLightIntensity;
+    GLint uKd;
+
+    //matrix
+    glm::mat4 ProjMatrix;
+    glm::mat4 MVMatrix;
+    glm::mat4 NormalMatrix;
+    glm::mat4 ViewMatrix;
+};
